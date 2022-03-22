8203accd6c6a3ecd5d3437ade9c5224377866245
heroyang11 <2639495046@qq.com>
2022-03-22 10:52:49 +0800

    Initial commit

---
+++ ch02-mybatis-dao/src/test/java/com/bjpowernode/TestMyBatis.java
@@ -0,0 +1,34 @@
+package com.bjpowernode;
+
+import com.bjpowernode.dao.StudentDao;
+import com.bjpowernode.dao.impl.StudentDaoImpl;
+import com.bjpowernode.domain.Student;
+import org.junit.Test;
+
+import java.util.List;
+
+public class TestMyBatis {
+
+    @Test
+    public void testSelectStudents(){
+
+        StudentDao dao  = new StudentDaoImpl();
+
+        List<Student> studentList  = dao.selectStudents();
+        for(Student stu:studentList){
+            System.out.println(stu);
+        }
+    }
+
+    @Test
+    public void testInsertStudent(){
+        StudentDao dao  = new StudentDaoImpl();
+        Student student = new Student();
+        student.setId(1005);
+        student.setName("盾山");
+        student.setEmail("dunshan@qq.com");
+        int nums = dao.insertStudent(student);
+        System.out.println("添加对象的数量："+nums);
+    }
+
+}
